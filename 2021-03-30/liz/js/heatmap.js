281f312d75d43f207519bb79342bc8d86610d31a
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2021-03-30 17:18:38 -0400

    look at stacked bar chat and heat map

--- 2021-03-30/liz/js/heatmap.js
+++ 2021-03-30/liz/js/heatmap.js
@@ -1,8 +1,8 @@
 function heatmap() {
   // set the dimensions and margins of the graph
-  var margin = {top: 80, right: 25, bottom: 30, left: 40},
-    width = 450 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+  var margin = {top: 80, right: 25, bottom: 30, left: 240},
+    width = 2000 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
   // append the svg object to the body of the page
   var svg = d3
@@ -14,16 +14,16 @@ function heatmap() {
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
   //Read the data
-  d3.csv('yoga-benefits-heat-map.csv', function (data) {
+  d3.csv('./data/yoga-benefits-heat-map.csv', function (data) {
     // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
     var myGroups = d3
       .map(data, function (d) {
-        return d.group;
+        return d['Sub-group'];
       })
       .keys();
     var myVars = d3
       .map(data, function (d) {
-        return d.variable;
+        return d['Yoga Pose'];
       })
       .keys();
 
@@ -32,73 +32,81 @@ function heatmap() {
     svg
       .append('g')
       .style('font-size', 15)
-      .attr('transform', 'translate(0,' + height + ')')
-      .call(d3.axisBottom(x).tickSize(0))
+      // .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisTop(x).tickSize(0))
       .select('.domain')
       .remove();
 
     // Build Y scales and axis:
     var y = d3.scaleBand().range([height, 0]).domain(myVars).padding(0.05);
-    svg.append('g').style('font-size', 15).call(d3.axisLeft(y).tickSize(0)).select('.domain').remove();
+    svg
+      .append('g')
+      .style('font-size', 15)
+      .call(d3.axisLeft(y).tickSize(0))
+      .select('.domain')
+      .remove();
 
     // Build color scale
-    var myColor = d3.scaleSequential().interpolator(d3.interpolateInferno).domain([1, 100]);
+    var myColor = d3
+      .scaleOrdinal()
+      .domain([0, 3])
+      .range(['transparent', 'red', 'blue', 'green', 'goldenrod']);
 
-    // create a tooltip
-    var tooltip = d3
-      .select('#pose-benefits')
-      .append('div')
-      .style('opacity', 0)
-      .attr('class', 'tooltip')
-      .style('background-color', 'white')
-      .style('border', 'solid')
-      .style('border-width', '2px')
-      .style('border-radius', '5px')
-      .style('padding', '5px');
+    // // create a tooltip
+    // var tooltip = d3
+    //   .select('#pose-benefits')
+    //   .append('div')
+    //   .style('opacity', 0)
+    //   .attr('class', 'tooltip')
+    //   .style('background-color', 'white')
+    //   .style('border', 'solid')
+    //   .style('border-width', '2px')
+    //   .style('border-radius', '5px')
+    //   .style('padding', '5px');
 
-    // Three function that change the tooltip when user hover / move / leave a cell
-    var mouseover = function (d) {
-      tooltip.style('opacity', 1);
-      d3.select(this).style('stroke', 'black').style('opacity', 1);
-    };
-    var mousemove = function (d) {
-      tooltip
-        .html('The exact value of<br>this cell is: ' + d.value)
-        .style('left', d3.mouse(this)[0] + 70 + 'px')
-        .style('top', d3.mouse(this)[1] + 'px');
-    };
-    var mouseleave = function (d) {
-      tooltip.style('opacity', 0);
-      d3.select(this).style('stroke', 'none').style('opacity', 0.8);
-    };
+    // // Three function that change the tooltip when user hover / move / leave a cell
+    // var mouseover = function (d) {
+    //   tooltip.style('opacity', 1);
+    //   d3.select(this).style('stroke', 'black').style('opacity', 1);
+    // };
+    // var mousemove = function (d) {
+    //   tooltip
+    //     .html('The exact value of<br>this cell is: ' + d.value)
+    //     .style('left', d3.mouse(this)[0] + 70 + 'px')
+    //     .style('top', d3.mouse(this)[1] + 'px');
+    // };
+    // var mouseleave = function (d) {
+    //   tooltip.style('opacity', 0);
+    //   d3.select(this).style('stroke', 'none').style('opacity', 0.8);
+    // };
 
     // add the squares
     svg
       .selectAll()
       .data(data, function (d) {
-        return d.group + ':' + d.variable;
+        return d['Sub-group'] + ':' + d['Yoga Pose'];
       })
       .enter()
       .append('rect')
       .attr('x', function (d) {
-        return x(d.group);
+        return x(d['Sub-group']);
       })
       .attr('y', function (d) {
-        return y(d.variable);
+        return y(d['Yoga Pose']);
       })
       .attr('rx', 4)
       .attr('ry', 4)
       .attr('width', x.bandwidth())
       .attr('height', y.bandwidth())
       .style('fill', function (d) {
-        return myColor(d.value);
+        return myColor(d['yes-no color value']);
       })
       .style('stroke-width', 4)
       .style('stroke', 'none')
-      .style('opacity', 0.8)
-      .on('mouseover', mouseover)
-      .on('mousemove', mousemove)
-      .on('mouseleave', mouseleave);
+      .style('opacity', 0.8);
+    // .on('mouseover', mouseover)
+    // .on('mousemove', mousemove)
+    // .on('mouseleave', mouseleave);
   });
 
   // Add title to graph
